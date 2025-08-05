187dbd3e088baa5747d082ceb5c5cb7ab36a915a
KainatBabar1 <F2024376515@umt.edu.pk>
2025-08-05 15:13:02 +0500

    first commit

---
+++ mini conda/first.py
@@ -0,0 +1,37 @@
+# # print("Kainat", end=' ')
+# # print("Babar")
+# # print("Kainat Babar")
+
+# # string and data type
+
+# a = '3'
+# b = '5'
+# print(a + b)
+# # string concationation
+# a = 'kainat' 
+# b = ' Babar'
+# print(a + b)
+# a= "kaiant"
+# print(f"i am {a}")
+
+#print 5.2
+# a = 5
+# b= str(0.2)
+# d = 0.2
+# c = a  + d
+# print(c
+
+
+#program to print the loop using to print the table of 2
+
+# # number = int(input("Enter number: "))
+# n = int(input('number :'))
+# for i in range(1, 11):
+#     # (1) ----> 10 
+#     # (n)  * i
+#     # print
+#     table = n * i
+#     print(f"{n} * {i} = {table}")
+#     if (table == 12):
+#         break
+    
